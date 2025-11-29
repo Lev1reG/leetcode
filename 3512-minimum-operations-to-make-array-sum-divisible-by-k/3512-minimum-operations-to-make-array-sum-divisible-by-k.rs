08c33d04a1cb903303ae4532c9018362fafcabca
Deren Tanaphan <109866900+Lev1reG@users.noreply.github.com>
2025-11-29 21:36:15 +0700

    Time: 0 ms (100%), Space: 2.2 MB (44%) - LeetHub

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let sum: i32 = nums.iter().sum();
+        let remainder = sum % k;
+
+        return remainder;
+    }
+}
