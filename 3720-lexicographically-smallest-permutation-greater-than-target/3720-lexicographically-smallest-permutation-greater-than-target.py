8663858431fa6db716218f4243caae271e4ec2fd
Deren Tanaphan <109866900+Lev1reG@users.noreply.github.com>
2026-08-27 15:58:14 +0700

    Time: 4 ms (93.42%), Space: 19.5 MB (42.11%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -2,36 +2,40 @@ class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         n = len(target)
         
-        for idx in range(n - 1, -1, -1):
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            
-            can_form_prefix = True
-            for c in target[:idx]:
-                if count[ord(c) - ord('a')] == 0:
-                    can_form_prefix = False
-                count[ord(c) - ord('a')] -= 1
-            
-            if not can_form_prefix:
-                continue
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        
+        match_len = 0
+        while match_len < n:
+            c_idx = ord(target[match_len]) - ord('a')
+            if count[c_idx] == 0:
+                break
+            count[c_idx] -= 1
+            match_len += 1
+        
+        if match_len == n:
+            match_len -= 1
+            count[ord(target[match_len]) - ord('a')] += 1
 
+        for idx in range(min(match_len, n - 1), -1, -1):
+            target_val = ord(target[idx]) - ord('a')
             greater_char = None
-            for i in range(ord(target[idx]) + 1, ord('z') + 1):
-                i -= ord('a')
+            for i in range(target_val + 1, 26):
                 if count[i] > 0:
                     greater_char = (chr(ord('a') + i))
                     count[i] -= 1
                     break
 
-            if greater_char is None:
-                continue
-
-            tail = []
-            for i in range(26):
-                if count[i] > 0:
-                    tail.append(chr(ord('a') + i) * count[i])
+            if greater_char is not None:
+                tail = []
+                for i in range(26):
+                    if count[i] > 0:
+                        tail.append(chr(ord('a') + i) * count[i])
+                
+                return target[:idx] + greater_char + "".join(tail)
             
-            return target[:idx] + greater_char + "".join(tail)
+            if idx > 0:
+                count[ord(target[idx - 1]) - ord('a')] += 1
             
         return ""
